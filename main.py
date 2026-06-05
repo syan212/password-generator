57b1b2e166e9a5674b1be3b7e3af904cfbc8aeaa
Siqi Yan <syan235711@proton.me>
2026-06-05 21:47:25 +0100

    intial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from rich import print as rprint
+from rich.console import Console
+
+import secrets
+
+console = Console()
+
+default_chars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890-=[];\'#,./!"$%^&*()_+{}:@~<>?'
+
+def generate_password(length, chars):
+    result = ''
+    for _ in range(length):
+        result += secrets.choice(chars)
+    return result
+
+length = console.input('[green]Enter length of password: [/]')
+try:
+    length = int(length)
+except ValueError:
+    rprint('[red]Error: length not a valid integer. Defaulting to 20.[/]')
+    length = 20
+
+use_default_chars = console.input('[green]Use default characters? ([/][light_green]y[/]/[red]n[/][green])[/] ').lower()
+
+chars = (
+    [char for char in default_chars] if use_default_chars == 'y' else 
+    [char for char in console.input('[green]Enter custom characters: [/]')] if use_default_chars == 'n' else
+    []
+)
+
+if not chars:
+    rprint('Invalid option or no characters entered. Using default characters.')
+    chars = [char for char in default_chars]
+
+rprint('[b green]Generated password[/]')
+rprint(f'[b green]{generate_password(length, chars)}[/]')
